3475a0cf6fc575efb83f36c523e1492af4d829b3
atacansuder <53799209+atacansuder@users.noreply.github.com>
2020-08-05 13:17:42 +0200

    Removed unneccessary files

--- junky/commands/chinese.js
+++
@@ -1,3 +0,0 @@
-exports.run = (client, message, args) => {
-    message.channel.send({files: ['https://i.redd.it/yzw9r2wmp2151.png']});
-}
--- junky/commands/send.js
+++
@@ -1,7 +0,0 @@
-exports.run = (client, message, args) => {
-
-    message.delete();
-    message.channel.send({files: ['https://i.kym-cdn.com/entries/icons/facebook/000/032/725/cover1.jpg']});
-    return;
-
-}
